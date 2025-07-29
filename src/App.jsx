6c322fb85c0e0e18ff70ac21689211d118d7a51e
abdussalam9891 <abdussalam9891@gmail.com>
2025-07-29 23:41:22 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import PlaceOrder from './pages/placeOrder/PlaceOrder'
+import Cart from './pages/cart/Cart'
+
+
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} /> 
+        <Route path='/cart' element={<Cart/>}/>
+          <Route path='/order' element={<PlaceOrder/>}/>
+      
+         
+      </Routes>
+     
     </div>
   )
 }
